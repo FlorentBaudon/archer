bc93ba1b3c7959cb1a8d0c5358edaa99af43dae1
florent.baudon@gmail.com <florent.baudon@gmail.com>
2015-12-16 08:12:31 +0100

    add locale + sddm + kcm-sddm

--- Archer/airootfs/root/install/configure.sh
+++ Archer/airootfs/root/install/configure.sh
@@ -10,8 +10,9 @@ rm /etc/systemd/system/getty@tty1.service.d/autologin.conf
 rm /root/{.automated_script.sh,.zlogin}
 rm /etc/mkinitcpio-archiso.conf
 rm -r /etc/initcpio
-
+#Language genration Eng Text Fr Layout
 locale-gen
+#localectl set-x11-keymap fr
 
 cp /root/install/syslinux.cfg /boot/syslinux/
 
@@ -20,9 +21,14 @@ syslinux-install_update -iam
 mkinitcpio -p linux
 
 systemctl enable dhcpcd
+systemctl enable sddm.service
 
 #initialisation de pacman
 pacman-key --init
 pacman-key --populate archlinux
+
+#Configure bash par defaut
+chsh -s /bin/bash root
+rm -rf /root/.zshrc /root/.zcompdump /root/.zsh_history
 
 rm -rf /root/install /root/install.txt /root/install.sh
